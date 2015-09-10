b5782a1e753ed5b5745eb7aeb55ce6a146f82baf
cole <cole@greybuntu>
2015-09-10 18:01:20 -0500

    Added Odometry System
    - moved motor test to new function
    - moved all tests into a callable function
    - added sample odometry controllers for 'precise' movement

--- RoboProjectOne/src/RobotTests.java
+++ RoboProjectOne/src/RobotTests.java
@@ -1,6 +1,8 @@
 import lejos.hardware.*;
 import lejos.hardware.motor.*;
 import lejos.hardware.sensor.*;
+import lejos.robotics.localization.*;
+import lejos.robotics.navigation.*;
 import lejos.robotics.SampleProvider;
 import lejos.robotics.filter.MeanFilter;
 import lejos.hardware.port.*;
@@ -10,36 +12,84 @@ import lejos.utility.Delay;
 public class RobotTests {
 
 	public static void main(String[] args) {
+		// runTests();
+		
+		// wheel diameter
+		double wheelDiameter = DifferentialPilot.WHEEL_SIZE_EV3;
+		// width between sides, I think?
+		double width = 15.2;
+		
+		// used for making 'precise' movements with robot
+		DifferentialPilot robot = new DifferentialPilot(wheelDiameter,width,Motor.C,Motor.B);
+		OdometryPoseProvider pp = new OdometryPoseProvider(robot);
+
+		// start position
+		System.out.println("Start: " + pp.getPose());
 		Button.waitForAnyPress();
 		
+		robot.rotate(90);
+		robot.travel(100);
+		robot.travel(-100);
+		robot.rotate(-90);
+		
+		// end position
+		System.out.println("End: " + pp.getPose());
+		Button.waitForAnyPress();
+		
+	}
+	
+	private void runTests(){
+		System.out.println("Press button!");
+		Button.waitForAnyPress();
+		
+		System.out.println("### IR Sensor ###");
 		// Test IR sensor on robot port #4,
 		//   query the sensor 10 times,
 		//   wait 1.5 seconds between queries
 		testIRSensor(SensorPort.S4, 10, 1500);
-		
+		Delay.msDelay(2000);
+
+		System.out.println("### Color Sensor ###");
 		// Test Color sensor on robot port #3,
 		//   query the sensor 10 times,
 		//   wait 1.5 seconds between queries
 		testColorSensor(SensorPort.S3, 10, 1500);
-		testIDColorSensor(SensorPort.S3, 10, 1500);
-		
+		Delay.msDelay(2000);
+
+		System.out.println("### Touch Sensor ###");
 		// Test for the touch sensor, port 2
 		// query sensor 10 times,
 		// wait 1.5 seconds between queries
-		testTouchSensor(SensorPort.S2, 10, 1500);
+		testTouchSensor(SensorPort.S2, 20, 1500);
+		Delay.msDelay(2000);
+
+		//System.out.println("### Ultrasonic Sensor ###");
+		// Test for the ultrasonic sensor, port 2
+		// query sensor 10 times,
+		// wait 1.5 seconds between queries
+		//testUltrasonicSensor(SensorPort.S1, 10 ,1500);
+		//Delay.msDelay(2000);		
 		
+		System.out.println("### Motors ###");
+		// Test motors on ports B and C
+		motorTest();
 		
+		System.out.println("Press button!");
+		Button.waitForAnyPress();
+	}
+	
+	private void motorTest(){
 		// Motor Code
 		UnregulatedMotor b = new UnregulatedMotor(MotorPort.B);
 		UnregulatedMotor c = new UnregulatedMotor(MotorPort.C);
-		b.setPower(10);
-		c.setPower(10);
-		
-		b.forward();
+		b.setPower(100);
+		c.setPower(100);
+				
+		b.backward();
 		c.forward();
 		// go forward for 1.5 seconds
 		Delay.msDelay(1500);
-		
+				
 		b.flt();
 		c.flt();
 		b.close();
@@ -49,7 +99,7 @@ public class RobotTests {
 	// Port: port that sensor is connected to
 	// iterations: number of times to query the sensor
 	// delay: number of milliseconds between iterations	
-	private static void testIDColorSensor(Port port, int iterations, long delay){
+	private static void testColorSensor(Port port, int iterations, long delay){
 		// get the IR sensor on the specific robot port
 		EV3ColorSensor sensor = new EV3ColorSensor(port);
 		
@@ -70,37 +120,7 @@ public class RobotTests {
 		((Device) sensor).close();
 	}
 
-	
-	// Port: port that sensor is connected to
-	// iterations: number of times to query the sensor
-	// delay: number of milliseconds between iterations	
-	private static void testColorSensor(Port port, int iterations, long delay){
-		// get the IR sensor on the specific robot port
-		EV3ColorSensor sensor = new EV3ColorSensor(port);
-		
-		// IR Sensor
-		for (int i=0; i < iterations; i++){
-			SampleProvider color = sensor.getRGBMode();
 
-			// stack a filter on the sensor that gives the running average of the last 5 samples
-			SampleProvider average = new MeanFilter(color, 5);
-
-			// initialize an array of floats for fetching samples
-			float[] sample = new float[average.sampleSize()];
-			
-			// fetch a sample
-			average.fetchSample(sample, 0);
-			
-			// Print newest sample value
-			System.out.println("("+sample[0]+","+sample[1]+","+sample[2]+")");
-			
-			// delay before next query to sensor
-			Delay.msDelay(delay);
-		}
-		
-		// close the sensor
-		((Device) sensor).close();
-	}
 	
 	// Port: port that sensor is connected to
 	// iterations: number of times to query the sensor
@@ -167,5 +187,33 @@ public class RobotTests {
 	}
 	
 	// code for ULTRA-Sonic found at http://sourceforge.net/p/lejos/wiki/Sensor%20Framework/
+	// Port: port that sensor is connected to
+	// iterations: number of times to query the sensor
+	// delay: number of milliseconds between iterations
+	private static void testUltrasonicSensor(Port port, int iterations, long delay){
+		// get the IR sensor on the specific robot port
+		SensorModes sensor = new EV3UltrasonicSensor(port);
+		
+		// IR Sensor
+		for (int i=0; i < iterations; i++){
+			SampleProvider distance= sensor.getMode("Distance");
+				// stack a filter on the sensor that gives the running average of the last 5 samples
+			SampleProvider average = new MeanFilter(distance, 5);
+				// initialize an array of floats for fetching samples
+			float[] sample = new float[average.sampleSize()];
+			
+			// fetch a sample
+			average.fetchSample(sample, 0);
+			
+			// Print newest sample value
+			System.out.println(sample[0]);
+			
+			// delay before next query to sensor
+			Delay.msDelay(delay);
+		}
+			
+		// close the sensor
+		((Device) sensor).close();
+	}
 
 }
